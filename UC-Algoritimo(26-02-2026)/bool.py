0659c8273aaa43bb035c289ad596d1ad319b816b
liimazz <guylhermegomeslima@gmail.com>
2026-02-26 14:38:31 -0300

    Add files via upload

---
+++ UC-Algoritimo(26-02-2026)/bool.py
@@ -0,0 +1,6 @@
+resp = input ("Você vai passarde ano? S/N: ").strip().lower()
+#resposta = bool (resp)
+resposta = (resp == "s")
+
+print ("Resposta do aluno ", resp)
+print ("Resultado ", resposta)
