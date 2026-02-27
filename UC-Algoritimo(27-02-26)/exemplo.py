8cb00d4ea077b5b4d0fb7017128d3cdb5bd2faa0
liimazz <guylhermegomeslima@gmail.com>
2026-02-27 16:10:27 -0300

    Add files via upload

---
+++ UC-Algoritimo(27-02-26)/exemplo.py
@@ -0,0 +1,12 @@
+altura =  float (input("Digite a sua altura: "))
+altura = altura * 100
+
+print (f"Minha altura é de {altura} cm")
+print ("Minha altura é de ", altura, "cm")
+
+nome = "Guilherme"
+idade = 17
+
+texto = "Meu nome é {} e tenho {} anos" .format(nome, idade)
+
+print (texto)
