0b431f37eb578919da0fbba007fdc8eb9e059e51
liimazz <guylhermegomeslima@gmail.com>
2026-03-04 11:23:55 -0300

    Add files via upload

---
+++ UC - Algoritimo(04-03-2026)/atividade.py
@@ -0,0 +1,9 @@
+COMPRAS = ["Arroz", "Feijão", "Leite", "Pão", "Ovos"]
+
+print("Lista atual:", COMPRAS)
+
+item = input("Digite um item para adicionar: ")
+
+COMPRAS.append(item)
+
+print("Lista atualizada:", COMPRAS)
