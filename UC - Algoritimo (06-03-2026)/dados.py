46cc281bfdd76b38c76bdfb58464bc78106fca26
liimazz <guylhermegomeslima@gmail.com>
2026-03-06 16:14:12 -0300

    Add files via upload

---
+++ UC - Algoritimo (06-03-2026)/dados.py
@@ -0,0 +1,11 @@
+# Quadardos de 1 a 10
+quadrados = [x**2 for x in range (1,11)]
+print ("Quadrados: ", quadrados)
+
+# Números pares de 1 a 20
+pares = [x for x in range (1,21) if x % 2 == 0]
+print ("Pares: ", pares)
+
+# Vogais de "PROGAMAÇÃO"
+vogais = [letra for letra in "PROGAMAÇÃO" if letra in "AEIOU"]
+print ("Vogais: ", vogais)
